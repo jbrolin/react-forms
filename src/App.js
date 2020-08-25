62f1bebe3525dddbfa3ea27a0141c2f437227f71
jbrolin <brolin.jonas@gmail.com>
2020-08-25 11:42:33 +0200

    created the basic form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [formValues, setFormValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
+  const handleFirstNameInputChange = (event) => {
+    event.persist();
+    setFormValues((formValues) => ({
+      ...formValues,
+      firstName: event.target.value,
+    }));
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-form">
+        <input
+          id="first-name"
+          class="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          value={formValues.firstName}
+          onChange={handleFirstNameInputChange}
+        />
+
+        <input
+          id="last-name"
+          class="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          value={formValues.lastName}
+        />
+
+        <input
+          id="email"
+          class="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={formValues.email}
+        />
+      </div>
     </div>
+
   );
 }
 
