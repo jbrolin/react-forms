cb5e8c6a9366cb1219b0583286ac1c9b5c51aa12
jbrolin <brolin.jonas@gmail.com>
2020-08-27 19:38:43 +0200

    Added logic for submit and feedback

--- src/App.js
+++ src/App.js
@@ -8,45 +8,67 @@ function App() {
     email: '',
   });
 
-  const handleFirstNameInputChange = (event) => {
+  const handleInputChange = (event) => {
     event.persist();
     setFormValues((formValues) => ({
       ...formValues,
-      firstName: event.target.value,
+      [event.target.name]: event.target.value,
     }));
+    // validate input
+    if ((formValues.firstName !=='') && (formValues.lastName !=='') && (formValues.email !=='')) {
+      setShowSubmit('true');
+    }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+};
+
+  const [showSubmit, setShowSubmit] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
   return (
     <div className="App">
       <div className="App-form">
+
+      <form className="form" onSubmit={handleSubmit}>
         <input
           id="first-name"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="First Name"
           name="firstName"
           value={formValues.firstName}
-          onChange={handleFirstNameInputChange}
+          onChange={handleInputChange}
         />
 
         <input
           id="last-name"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Last Name"
           name="lastName"
           value={formValues.lastName}
+          onChange={handleInputChange}
         />
 
         <input
           id="email"
-          class="form-field"
+          className="form-field"
           type="text"
           placeholder="Email"
           name="email"
           value={formValues.email}
+          onChange={handleInputChange}
         />
+         <button className="form-submit" type="submit" disabled={!showSubmit}>
+          Register
+        </button>
+
+        {submitted && <div className='form-message'>Thank you!</div>}
+
+        </form>
       </div>
     </div>
 
